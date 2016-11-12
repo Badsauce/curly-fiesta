b1b6d90da0dacf78b1e1b5b27464944ac3af980f
Nick Rutowski <nickr@doordash.com>
2016-11-12 14:28:09 -0800

    basic youtube video player

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import YouTube from 'react-youtube';
 
 class App extends Component {
   render() {
+    const opts = {
+      width: '960',
+      height: '540',
+      playerVars: { // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+        controls: 0,
+      },
+    }
+
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>D007Dash</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <YouTube
+            videoId="QO5WZMyvNoY"
+            opts={opts}
+            onEnd={() => {alert('Done!')}}
+          />
         </p>
       </div>
     );
